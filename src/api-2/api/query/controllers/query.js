aace8fe2a5f975b2eb2ef4b5117330420e4cb7e0
denny <denny.george90@gmail.com>
2020-07-28 07:47:55 +0530

    (feat) add feedback API and updated documentation files

--- src/api-2/api/query/controllers/query.js
+++ src/api-2/api/query/controllers/query.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async feedback(ctx) {
+    const { id } = ctx.params;
+    const { value } = ctx.request.body;
+    // const value = "positive";
+    strapi.log.debug(id);
+    const query = await strapi.services.query.findOne({ id });
+    strapi.log.debug(query);
+    query.feedback = value;
+    const updatedValue = await strapi.services.query.update({ id }, query);
+    return sanitizeEntity(updatedValue, { model: strapi.models.query });
+  },
+};
