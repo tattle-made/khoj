02a47d42c3e38f1003774e6cfa25052ef066209d
denny <denny.george90@gmail.com>
2020-08-09 21:47:06 +0530

    (fix) correct feedback API by changing parameter from feedback to user_feedback

--- src/api-2/api/query/controllers/query.js
+++ src/api-2/api/query/controllers/query.js
@@ -10,13 +10,30 @@ module.exports = {
   async feedback(ctx) {
     const { id } = ctx.params;
     const { value } = ctx.request.body;
-    // const value = "positive";
-    strapi.log.debug(id);
-    const query = await strapi.services.query.findOne({ id });
-    strapi.log.debug(query);
-    query.feedback = value;
-    const updatedValue = await strapi.services.query.update({ id }, query);
-    return sanitizeEntity(updatedValue, { model: strapi.models.query });
+    // const value = "positive";\
+    strapi.log.debug(" Feedback Endpoint Pinged");
+    strapi.log.debug("id : " + id + " | value : " + value);
+    strapi.log.debug(strapi);
+
+    return strapi.services.query
+      .findOne({ id })
+      .then((query) => {
+        query.user_feedback = value;
+        return strapi.services.query
+          .update({ id }, query)
+          .then((updatedQuery) => {
+            return sanitizeEntity(updatedQuery, { model: strapi.models.query });
+          });
+      })
+      .catch((err) => {
+        console.log("error", err);
+        return "error";
+      });
+    // const query = await strapi.services.query.findOne({ id });
+    // strapi.log.debug(query);
+    // query.feedback = value;
+    // const updatedValue = await strapi.services.query.update({ id }, query);
+    // return sanitizeEntity(updatedValue, { model: strapi.models.query });
   },
 
   async create(ctx) {
