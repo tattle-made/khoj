ea960897c108c0bef31b42a38329af419153db2d
denny <denny.george90@gmail.com>
2020-09-08 16:18:22 +0530

    (feat) auto create text response upon query creation

--- src/api-2/api/query/models/query.js
+++ src/api-2/api/query/models/query.js
@@ -9,6 +9,20 @@ module.exports = {
   lifecycles: {
     async afterCreate(result, data) {
       console.log("post create");
+      console.log(result);
+      console.log(data);
+      const queryId = result["_id"];
+      data = {
+        type: "text",
+        text: {
+          heading: "Thank you for sending us your query.",
+          byline:
+            "Our team is looking into your request now. Please give us 3-5 business days to get back to you.",
+        },
+        queries: [queryId],
+      };
+      const response = await strapi.services.response.create(data);
+      console.log(response);
     },
   },
 };
