7514f2405c614d6ee1bb9746ea3096381c635977
denny <denny.george90@gmail.com>
2020-09-08 23:39:02 +0530

    (fix)

--- src/api-2/api/response/controllers/response.js
+++ src/api-2/api/response/controllers/response.js
@@ -1,6 +1,8 @@
 "use strict";
 const admin = require("firebase-admin");
 
+var appInitialized = false;
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
@@ -8,11 +10,14 @@ const admin = require("firebase-admin");
 
 module.exports = {
   async notify(ctx) {
-    var serviceAccount = require("./firebase-keys.json");
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: "https://tattle-khoj.firebaseio.com",
-    });
+    if (!appInitialized) {
+      var serviceAccount = require("./firebase-keys.json");
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://tattle-khoj.firebaseio.com",
+      });
+      appInitialized = true;
+    }
 
     const { token, title, byline, queryId } = ctx.request.body;
     console.log({ token, title, byline, queryId });
