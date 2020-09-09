f34e127155c205116cd266ec2dceeaf1385668ce
denny <denny.george90@gmail.com>
2020-09-09 08:10:42 +0530

    (feat) configure s3 deploy plugin

--- src/ui-web-community/README.md
+++ src/ui-web-community/README.md
@@ -1,1 +1,1 @@
-changed ui-web-user v0.0.4
+changed ui-web-user v0.0.5
--- src/ui-web-community/gatsby-config.js
+++ src/ui-web-community/gatsby-config.js
@@ -44,6 +44,12 @@ module.exports = {
         icon: `src/images/tattle_logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "khoj.tattle.co.in",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
